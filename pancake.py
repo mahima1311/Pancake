3592588280d977dd2ccf9a5e53e5878013be6fc3
mahima1311 <49207880+mahima1311@users.noreply.github.com>
2026-01-21 17:03:27 +0530

    Update pancake.py

--- pancake.py
+++ pancake.py
@@ -9,9 +9,10 @@ pancake_css = """
         position: relative;
         height: 150px; /* Set height to prevent layout collapse */
     }
-.pancake-one {
+.pancake{
     position: absolute;
     left: 50%;
+    top: 0px;
     transform: translateX(-50%);
     background-color: #DEB887; /* Golden brown */
     border-radius: 50%;
@@ -22,36 +23,6 @@ pancake_css = """
     border: 5px outset #8B4513; /* Darker edge */
     box-shadow: 0px 5px 5px rgba(139, 69, 19, 1);
     filter: drop-shadow(0px 5px 5px rgba(139, 69, 19, 1));
-}
-.pancake-two {
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #DEB887; /* Golden brown */
-    border-radius: 50%;
-    width: 200px;
-    height: 100px;
-    margin: 10px auto;
-    z-index: 2;
-    border: 5px outset #8B4513; /* Darker edge */
-    box-shadow: 0px 5px 5px rgba(139, 69, 19, 1);
-    filter: drop-shadow(0px 5px 5px rgba(139, 69, 19, 1));
-    top: 20px;
-}
-.pancake-three {
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #DEB887; /* Golden brown */
-    border-radius: 50%;
-    width: 200px;
-    height: 100px;
-    margin: 10px auto;
-    z-index: 1;
-    border: 5px outset #8B4513; /* Darker edge */
-    box-shadow: 0px 5px 5px rgba(139, 69, 19, 1);
-    filter: drop-shadow(0px 5px 5px rgba(139, 69, 19, 1));
-    top: 40px;
 }
 .butter {
     position: absolute;
@@ -73,19 +44,20 @@ pancake_css = """
 st.markdown(pancake_css, unsafe_allow_html=True)
 
 # Generate a stack
-# num_pancakes = st.slider("Stack Size", 1, 5, 3)
-# for i in range(num_pancakes):
-#     st.markdown('<div class="pancake"></div>', unsafe_allow_html=True)
-#     st.markdown('<div class="butter"></div>', unsafe_allow_html=True)
+num_pancakes = st.slider("Stack Size", 1, 1000, 3)
+st.markdown('<div class="parent-container">', unsafe_allow_html=True)
+st.markdown('<div class="butter"></div>', unsafe_allow_html=True)
+for i in range(num_pancakes):
+     st.markdown(f'<div class="pancake" style="top: {i * 20}px;"></div>', unsafe_allow_html=True)
 
 
 
-st.markdown("""
-<div class="parent-container">
-    <div class="butter"></div>
-    <div class="pancake-one"></div>
-    <div class="pancake-two"></div>
-    <div class="pancake-three"></div>
-</div>
-""", unsafe_allow_html=True)
+#st.markdown("""
+#<div class="parent-container">
+#    <div class="butter"></div>
+#    <div class="pancake-one"></div>
+#    <div class="pancake-two"></div>
+#    <div class="pancake-three"></div>
+#</div>
+#""", unsafe_allow_html=True)
 
